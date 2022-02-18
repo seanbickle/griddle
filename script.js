18b1d45896c56acf5336b998d92b3531356e6518
Sean Bickle <sean.a.bickle@gmail.com>
2022-02-18 10:38:38 +0000

    feat: Enable user selection
    Added methods to select characters and get/submit words, plus a number
    of helper methods.
    
    Switched to a 1D grid. This makes it slightly harder to shift letters
    down later on, and to check the congruity of the letters, but it does
    make it easier to deduplicate the selection. I'm still not sold on 1D
    over 2D so this might change.

--- script.js
+++ script.js
@@ -4,19 +4,36 @@ class WordHandler{
     wordlist = wordlist
     chars = "abcdefghijklmnopqrstuvwxyz"
     grid = []
+    user_selection = []
 
     constructor(){
         this._init_grid()
     }
 
+    // INTERFACE
+    select(index){
+        this._toggle_select(index)
+    }
+
+    submit_word(){
+        // Triggered by user entering selection
+        return this._in_word_list(this.word())
+    }
+    
+    word(){
+        // Convert user selection to word
+        var word = ""
+        for(var i = 0; i < this.user_selection.length; i++){
+            word += this.grid[this.user_selection[i]]
+        }
+        return word
+    }
+
+    // HELPERS
     _init_grid(){
-        // Pushes GRID_HEIGHT rows of GRID_WIDTH letters to grid
-        for(var x = 0; x < this.GRID_HEIGHT; x++){
-            var row = []
-            for(var y = 0; y < this.GRID_WIDTH; y++){
-                row.push(this._get_rand_char())
-            }
-            this.grid.push(row)
+        // Pushes GRID_HEIGHT * GRID_WIDTH letters to grid
+        for(var i = 0; i < this.GRID_WIDTH * this.GRID_HEIGHT; i++){
+            this.grid.push(this._get_rand_char())
         }
     }
 
@@ -29,4 +46,32 @@ class WordHandler{
         // Random int between 0 and 25
         return Math.floor(Math.random() * this.chars.length)
     }
+
+    _in_word_list(word){
+        // Checks word in word list
+        return this.wordlist.includes(word)
+    }
+
+    _toggle_select(index){
+        // If index already selected, remove. Else, add.
+        if(this._already_selected(index)) this._remove_selection(index)
+        else this._add_selection(index)
+    }
+
+    _already_selected(index){
+        // If grid index is already in the user's selection
+        return this.user_selection.includes(index)
+    }
+
+    _add_selection(index){
+        // Add grid index to user selection
+        this.user_selection.push(index)
+    }
+
+    _remove_selection(index){
+        // Remove grid index from user selection
+        this.user_selection.splice(this.user_selection.indexOf(index))
+    }
 }
+
+var wh = new WordHandler()
