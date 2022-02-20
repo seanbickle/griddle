f3dd6e3bae08a7b816f5f915095a77153fa10f08
Sean Bickle <sean.a.bickle@gmail.com>
2022-02-20 21:06:24 +0000

    improvement(errors): Update copy
    This makes it a bit easier to read in the toast.

--- errors.js
+++ errors.js
@@ -1,13 +1,13 @@
 class NotInWordListError extends Error{
     constructor(word){
-        super("Word not in list: " + word)
+        super(word + " is not in the word list")
         this.name = "NotInWordListError"
     }
 }
 
 class IncongruousSelectionError extends Error{
     constructor(){
-        super("Selected tiles must be congruous with the last selection")
+        super("selected tiles must be congruous with the last selection")
         this.name = "IncongruousSelectionError"
     }
 }
